da98d958198db4115e9c0b002dfd671ad0d0ea21
jairoHer <32321394+jairoHer@users.noreply.github.com>
2018-08-24 00:21:32 -0600

    se creo la funcion factorial 201404423

---
+++ calculadora.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class calculadora {
+	public static void main(String[] args){
+		//menu();
+		Scanner input=new Scanner(System.in);
+		System.out.println("Seleccione una opcion");
+		System.out.println("1. factorial");
+		System.out.println("2. Salir");
+		int opcion = input.nextInt();
+		while (opcion!=2){
+			switch (opcion){
+				case 1:
+					System.out.println("-----Factorial------");
+					System.out.println("Ingrese un numero");
+					int facto = input.nextInt();
+					System.out.println("El factorial es: "+factorial(facto));
+					menu();
+					opcion = input.nextInt();
+					break;
+				default:
+					System.out.println("Opcion no valida");
+					menu();
+					opcion = input.nextInt();
+					break;
+			}
+		}
+		
+	}
+	public static void menu(){
+		System.out.println("Seleccione otra opcion");
+		System.out.println("1. factorial");
+		System.out.println("2. Salir");
+		
+	}
+	public static int factorial(int numero){
+		int facto=1;
+		while (numero!=0){
+			facto = facto*numero;
+			numero--;
+		}
+		return facto;
+	}
+}
