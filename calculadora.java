967b106a67a579c1714567ea07b2ed6cb3b9ed69
harias25 <ihpam25@gmail.com>
2018-08-24 11:54:00 -0600

    Update calculadora.java
    Se creo la función la funcion raiz y se agrego en el menú principal.
    201020247

--- calculadora.java
+++ calculadora.java
@@ -6,15 +6,24 @@ public class calculadora {
 		Scanner input=new Scanner(System.in);
 		System.out.println("Seleccione una opcion");
 		System.out.println("1. factorial");
-		System.out.println("2. Salir");
+		System.out.println("2. raiz cuadrada");
+		System.out.println("3. Salir");
 		int opcion = input.nextInt();
-		while (opcion!=2){
+		while (opcion!=3){
 			switch (opcion){
 				case 1:
 					System.out.println("-----Factorial------");
 					System.out.println("Ingrese un numero");
 					int facto = input.nextInt();
 					System.out.println("El factorial es: "+factorial(facto));
+					menu();
+					opcion = input.nextInt();
+					break;
+				case 2:
+					System.out.println("-----Raíz Cuadrada------");
+					System.out.println("Ingrese un numero");
+					int num = input.nextInt();
+					System.out.println("La raíz cuadrada del numero es es: "+raiz(num));
 					menu();
 					opcion = input.nextInt();
 					break;
@@ -45,5 +54,11 @@ public class calculadora {
 	public static int potencia(int numero){
 		int potencia = numero * numero;
 		return potencia;
+	}
+	
+	public static float raiz(int numero)
+	{
+		float resultado1 = (float) Math.sqrt(numero);
+		return resultado1;
 	}
 }
